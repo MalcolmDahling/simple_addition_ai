a796031bb0b3f591c2f1cbb4dcf7761c19f2662f
Malcolm Dahling <mac.dahling@gmail.com>
2025-11-15 02:15:19 +0100

    mobile styling

--- src/components/main/main.css.ts
+++ src/components/main/main.css.ts
@@ -2,8 +2,8 @@ import { recipe } from '@vanilla-extract/recipes';
 
 export const MainStyle = recipe({
   base: {
-    position: 'absolute',
-    inset: 20,
+    minHeight: '100vh',
+    padding: 20,
 
     display: 'flex',
     justifyContent: 'center',
